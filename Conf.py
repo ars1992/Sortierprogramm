e5aaa2afdae087e74f07768095b791cdea04611d
ars1992 <alessandro.salomon@gmx.de>
2023-08-11 12:48:42 +0200

    funktion für einstellungs auswahl

--- Conf.py
+++ Conf.py
@@ -1,5 +1,13 @@
 class Conf:
     def __init__(self, pfad):
-        self.pfad = pfad
+        self.__pfad = pfad
+
+    def auswahl_einstellungen(self):
+        print("Was möchtest du tun?")
+        print("1 - Neuer Quellordner anlegen")
+        print("2 - Neuer Zielordner anlegen")
+        auswahl = input("Deine Wahl: ")
+
 
     def conf_erstellen(self):
+        ...
