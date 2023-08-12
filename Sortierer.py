486d4688abae4ac092868b3bdb8e1b5fedbc326b
ars1992 <alessandro.salomon@gmx.de>
2023-08-12 14:27:45 +0200

    Fixes #1 Dateien können jetzt bei felerhaftem dateiname umbenannt werden

--- Sortierer.py
+++ Sortierer.py
@@ -14,9 +14,15 @@ class Sortierer:
         return f"{pfad}\\{dateiname}"
 
     def __datei_verschieben(self, dateiname="", ordner="", index=0):
-        if dateiname.split("_")[1].upper() == ordner.upper():
-            shutil.move(self.__pfad_erstellen(dateiname=dateiname, pfad=self.__quellordner[index]),
-                        self.__pfad_erstellen(dateiname=dateiname, pfad=self.__zielordner[ordner]))
+        try:
+            if dateiname.split("_")[1].upper() == ordner.upper():
+                shutil.move(self.__pfad_erstellen(dateiname=dateiname, pfad=self.__quellordner[index]),
+                            self.__pfad_erstellen(dateiname=dateiname, pfad=self.__zielordner[ordner]))
+        except IndexError:
+            print(f"Felerhafterdateiname von: {dateiname}")
+            eingabe = input("Möchtest du die Datei umbenennen (j/n): ")
+            if eingabe.upper() == "J":
+                self.datei_umbenennen(dateiname=dateiname)
 
     def zielordner_durchlaufen_und_einsortieren(self):
         for i in range(0, len(self.__quellordner)):
@@ -24,3 +30,11 @@ class Sortierer:
             for dateiname in files:
                 for ordner, pfad in self.__zielordner.items():
                     self.__datei_verschieben(dateiname=dateiname, ordner=pfad.split("\\")[-1], index=i)
+
+    def datei_umbenennen(self, dateiname=None, index=0):
+        if dateiname:
+            print(f"Aktuellername: {dateiname}")
+            neuer_dateiname = input("Neuer Dateiname: ")
+            os.rename(f"{self.__quellordner[index]}\\{dateiname}",
+                      f"{self.__quellordner[index]}\\{neuer_dateiname}")
+            self.zielordner_durchlaufen_und_einsortieren()
